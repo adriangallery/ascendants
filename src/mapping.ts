08a4a65fa2778d2cc372a8ffcabc304a46c4209f
ADRIAN <adriangallery@adriancerda.com>
2025-02-28 08:05:47 +0100

    Update mapping.ts

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,18 +1,39 @@
-import { TransferSingle, TransferBatch } from "../generated/AscendantsDapp/AscendantsDapp";
-import { Transfer } from "../generated/schema";
+import { TransferSingle, TransferBatch } from "../generated/AdrianGallery1155/AdrianGallery1155";
+import { Transfer, Holder, TokenBalance } from "../generated/schema";
 import { BigInt } from "@graphprotocol/graph-ts";
 
+/**
+ * handleTransferSingle
+ * Se ejecuta cuando se emite un evento TransferSingle.
+ */
 export function handleTransferSingle(event: TransferSingle): void {
   let id = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
-  let entity = new Transfer(id);
-  entity.from = event.params.from;
-  entity.to = event.params.to;
-  entity.tokenId = event.params.id;
-  entity.value = event.params.value;
-  entity.timestamp = event.block.timestamp;
-  entity.save();
+  let transfer = new Transfer(id);
+  transfer.from = event.params.from;
+  transfer.to = event.params.to;
+  transfer.tokenId = event.params.id;
+  transfer.value = event.params.value;
+  transfer.timestamp = event.block.timestamp;
+  transfer.save();
+
+  // Aquí puedes agregar lógica adicional para actualizar balances o registrar propietarios.
 }
 
+/**
+ * handleTransferBatch
+ * Se ejecuta para eventos TransferBatch.
+ */
 export function handleTransferBatch(event: TransferBatch): void {
-  // Lógica para TransferBatch
+  for (let i = 0; i < event.params.ids.length; i++) {
+    let id = event.transaction.hash.toHex() + "-" + event.logIndex.toString() + "-" + i.toString();
+    let transfer = new Transfer(id);
+    transfer.from = event.params.from;
+    transfer.to = event.params.to;
+    transfer.tokenId = event.params.ids[i];
+    transfer.value = event.params.values[i];
+    transfer.timestamp = event.block.timestamp;
+    transfer.save();
+
+    // Aquí también puedes agregar lógica para actualizar balances o propietarios.
+  }
 }
